b06cb5ecf7b7e3980bb23f1984dfc6d089e23e7d
peterdevpo <peter@paracosma.com>
2024-04-25 20:36:17 +0545

    CSS update

--- components/FormEntry.jsx
+++ components/FormEntry.jsx
@@ -16,28 +16,28 @@ const FormEntry = () => {
   return (
     <>
     <div className=' flex justify-center items-center'>
-    <form className='max-w-screen-xl w-1/3' >
-      <div className='grid grid-cols-2 gap-3 my-4'>
+    <form className='max-w-screen-xl w-1/1.5' >
+      <div className='form-lable-group'>
       <label> Type of water users </label>
-      <select className='p-1 '>
+      <select className='p-1 border-input-form'>
         <option value="Owner">Owner</option>
         <option value="Rental">Rental</option>
       </select>
       </div>
 
-      <div className='grid grid-cols-2 gap-3 my-4 items-center'>
+      <div className='form-lable-group '>
       <label> Name of Users </label>
-      <input type="text" className='border border-solid border-gray-500 p-1 rounded-md' placeholder='Your Name'/>
+      <input type="text" className='border-input-form' placeholder='Your Name'/>
       </div>
 
-      <div className='grid grid-cols-2 gap-3 my-4 items-center'>
+      <div className='form-lable-group'>
       <label> Citizenship </label>
       <input type="text" className='border border-solid border-gray-500 p-1 rounded-md' placeholder='Citizenship'/>
       </div>
 
-      <div className='grid grid-cols-2 gap-3 my-4 items-center'>
+      <div className='form-lable-group'>
       <label> Palika </label>
-      <select onChange={handelPalikaChange} className='p-1 '>
+      <select onChange={handelPalikaChange} className='p-1 border-input-form'>
         <option value="Kamal">Kamal</option>
         <option value="Gauriganj">Gauriganj</option>
         <option value="Gauradaha">Gauradaha</option>
@@ -45,15 +45,27 @@ const FormEntry = () => {
       </select>
       </div>
 
-      <div className='grid grid-cols-2 gap-3 my-4 items-center'>
+      <div className='form-lable-group'>
       <label> Secondary  </label>
-      <select className='p-1 '>
+      <select className='p-1 border-input-form'>
         {palikaCa.map((item)=>{
           return <option key={item} value={item}>{item}</option>
         })}
       </select>
       </div>
 
+      <div className="land-cal">
+      <div className='form-lable-group'>
+      <label> Number of Land </label>
+      <select className='p-1 border-input-form'>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      </div>
+      </div>
 
       
     </form>
