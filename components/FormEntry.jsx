2e98074e0cfa8d1643f8d0f13a32d3db7d2db296
Pinological <captainpeter70@gmail.com>
2024-04-25 22:15:32 +0545

    added kattha

--- components/FormEntry.jsx
+++ components/FormEntry.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { palikaSCData } from '@/utility/data'
 
@@ -8,14 +8,21 @@ const FormEntry = () => {
 
   const [palikaCa , setPalikaCa] = useState(palikaSCData.Kamal)
 
+  const[numberOfLand, setNumberOfLand] = useState(1)
+
   const handelPalikaChange = (e) =>{
 
     setPalikaCa(palikaSCData[e.target.value])
   }
 
+  const handleNoOfLandChange = (e) => {
+    setNumberOfLand(parseInt(e.target.value))
+  }
+
+
   return (
     <>
-    <div className=' flex justify-center items-center'>
+    <div className=' flex justify-center items-center mx-2'>
     <form className='max-w-screen-xl w-1/1.5' >
       <div className='form-lable-group'>
       <label> Type of water users </label>
@@ -55,16 +62,35 @@ const FormEntry = () => {
       </div>
 
       <div className="land-cal">
-      <div className='form-lable-group'>
-      <label> Number of Land </label>
-      <select className='p-1 border-input-form'>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      </div>
+        <div className='form-lable-group'>
+        <label> Number of Land </label>
+          <select onChange={handleNoOfLandChange} className='p-1 border-input-form'>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+
+        <div className=" grid grid-cols-4 ">
+          <p className=' border border-solid border-gray-500 p-1'>No of Land</p>
+          <p className=' border border-solid border-gray-500 p-1'>Bigha</p>
+          <p className=' border border-solid border-gray-500 p-1'>Kattha</p>
+          <p className=' border border-solid border-gray-500 p-1'>Dhur</p>
+        </div>
+
+        {[...Array(numberOfLand)].map((_,index)=>{
+          return(
+              <div key={index} className=" my-3 grid grid-cols-4 ">
+                <span className='text-center p-1'> Land : {index + 1}</span>
+                <input type="number" placeholder='Bigha' className=' mx-2 border-input-form'/>
+                <input type="number" placeholder='Kattha' className='mx-2 border-input-form'/>
+                <input type="number" placeholder='Dhur' className='mx-2 border-input-form'/>
+              </div>
+          )
+        })}
+
       </div>
 
       
